8c3278f216308ce3732035ece4ada2cc7447223c
Melvin <melvin.lin.work@gmail.com>
2025-08-25 12:45:52 +0700

    chore: tidy up project structure
    - Separate finished and unfinished projects into dedicated sections
    - Add archive folder for older versions
    - Remove unused files and folders
    - Add detailed README with game list and sections

--- Othello/2/Player Vs Player/assets/js/grid.js
+++
@@ -1,8 +0,0 @@
-function generateGrid(){
-	for(let row = 0; row < length; row++){
-		gameGrid.push([]);
-		for(let col = 0; col < length; col++){
-			gameGrid[row].push(new Tile(col * size, row * size));
-		}
-	}
-}
