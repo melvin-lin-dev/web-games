213878a0c49fc48511de3926b18feee519906f24
Melvin <melvin.lin.work@gmail.com>
2025-08-26 12:19:22 +0700

    chore: remove star-battle folder

--- star-battle/instructions.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-  let items = $('#instructions *');
-
-  for (let i = 0; i < items.length; i++) {
-    setTimeout( function () {
-      // $(items[i]).addClass('animate');
-    }, i * 25)
-  }
-});
